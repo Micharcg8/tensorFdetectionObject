7f8ada1b05941d58ac3a27af723937fe42ec0c77
Michael <michaelrcg8@gmail.com>
2023-11-14 09:16:55 -0300

    upload files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as cocossd from '@tensorflow-models/coco-ssd';
+import './App.css'; // Importa el archivo CSS
 
 function App() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const runObjectDetection = async () => {
+
+      const net = await cocossd.load();
+
+      const video = videoRef.current;
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      video.srcObject = stream;
+
+      const detectObjects = async () => {
+        const predictions = await net.detect(video);
+        drawPredictions(predictions);
+        requestAnimationFrame(detectObjects);
+      };
+
+      video.addEventListener('loadedmetadata', () => {
+        video.play();
+        detectObjects();
+      });
+    };
+
+    runObjectDetection();
+  }, []);
+
+  const drawPredictions = (predictions) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const video = videoRef.current;
+    const scaleFactor = canvas.width / video.width;
+
+    predictions.forEach((prediction) => {
+      const [x, y, width, height] = prediction.bbox;
+      const label = `${prediction.class} (${Math.round(prediction.score * 100)}%)`;
+
+      const adjustedX = x * scaleFactor;
+      const adjustedY = y * scaleFactor;
+      const adjustedWidth = width * scaleFactor;
+      const adjustedHeight = height * scaleFactor;
+
+      ctx.strokeStyle = '#00FFFF';
+      ctx.lineWidth = 2;
+      ctx.strokeRect(adjustedX, adjustedY, adjustedWidth, adjustedHeight);
+
+      ctx.fillStyle = '#00FFFF';
+      ctx.fillRect(adjustedX, adjustedY - 20, label.length * 8, 20);
+      ctx.fillStyle = '#000000';
+      ctx.fillText(label, adjustedX, adjustedY - 5);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video ref={videoRef} autoPlay playsInline width="640" height="480" />
+      <canvas ref={canvasRef} width="640" height="480" className="object-canvas" />
     </div>
   );
 }
